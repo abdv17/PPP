177b2b24fcde8031c767e9bc4934d01e9606a769
Sai Gaddam <sg8412@outlook.com>
2026-01-28 07:12:25 +0000

    Modified the test case

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import sync_playwright
 
 def test_login():
+    print('running login test case')
     with sync_playwright as p:
+        print('Inside sync playwright block and launching chrome with google.co.in')   
         browser = p.chromium.launch(headless=False)
         context = browser.new_context(viewport=None)
         page = content.new_page()
