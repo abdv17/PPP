0ebeeb733b358c1b3467053089f5c3ff34ba8401
Sai.Gaddam <saikumar.gaddam@cigniti.com>
2026-01-28 20:58:57 +0530

    changed logging mechanism

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,19 +8,10 @@ def test_login():
     LOG_DIR = "Log"
     os.makedirs(LOG_DIR, exist_ok=True)
     log_path = os.path.join(LOG_DIR, f"test_{datetime.now():%Y-%m-%d}.log")
-    
-    logging.basicConfig(
-        level=logging.INFO,                   # DEBUG | INFO | WARNING | ERROR | CRITICAL
-        filename=log_path,                    # <- this creates/writes to file
-        filemode="a",                         # "w" to overwrite each run
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
+    with open(log_path,"a") as p:
+        p.write('This is dummy log')
 
-    
-    logging.info('Running the test case')
-    print('running login test case')
-    logging.debug('')
+
     # with sync_playwright as p:
     #     logging.info('Inside sync playwright block')
     #     print('Inside sync playwright block and launching chrome with google.co.in')
